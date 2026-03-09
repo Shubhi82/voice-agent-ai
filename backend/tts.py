f5c67814eb192f005013372806d675c5714be6a7
Shubhi Vashistha <82753753+Shubhi82@users.noreply.github.com>
2026-03-09 20:59:08 +0530

    Add text-to-voice functionality using TTS API

--- backend/tts.py
+++ backend/tts.py
@@ -1,1 +1,7 @@
+from TTS.api import TTS
 
+tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC")
+
+def text_to_voice(text, output="response.wav"):
+    tts.tts_to_file(text=text, file_path=output)
+    return output
